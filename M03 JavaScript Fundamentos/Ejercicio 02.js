71fb8ff71345d4cd3b632bb7117702d8336d5c02
Jessica Sanchez <jessicasanz24@outlook.com>
2023-09-01 19:02:32 -0200

    Henry Test03

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -1,36 +1,42 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*2️⃣ EJERCICIO 02 2️⃣*/
 
-function devolverString(string) {
-   // Debe retornar un string.
-   // Tu código:
-}
+function devolverString() {
+   return "Henry"; {
+      devolverString("Henry");
+   }
+   }
 
-// ⛔️ "X" e "Y" son números.
+// ⛔️ "X" e "Y" son númros.
 
 function suma(x, y) {
-   // Retorna el resultado de su suma.
-   // Tu código:
+   return x + y;{
+      suma(3, 8);
+   }
 }
 
 function resta(x, y) {
-   // Retorna el resultado de la resta.
-   // Tu código:
+   return x - y;{
+      resta(10, 5);
+   }
 }
 
 function divide(x, y) {
-   // Retorna el resultado de su división.
-   // Tu código:
+   return x / y; {
+      divide(20, 2);
+   }
 }
 
 function multiplica(x, y) {
-   // Retorna el resultado de su multiplicación.
-   // Tu código:
+   return x * y;{
+      multiplica(5, 4);
+   }
 }
 
 function obtenerResto(x, y) {
-   // Obten el resto de la división de "x" entre "y".
-   // Tu código:
+   return x % y;{
+   obtenerResto(40, 3);
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -2,40 +2,66 @@
 /*3️⃣ EJERCICIO 03 3️⃣*/
 
 function sonIguales(x, y) {
-   // Retorna true si "x" e "y" son iguales.
-   // De lo contrario, retorna false.
-   // Tu código:
-}
+    if (x ===y) {
+      return(true)
+     } else (x !=y);{
+      return(false)
+     }
+   }
+   sonIguales(50, 90)
+   sonIguales(10, 10)
+  
 
 function tienenMismaLongitud(str1, str2) {
-   // Retorna true si los dos strings tienen la misma longitud.
-   // De lo contrario, retorna false.
-   // Tu código:
-}
+   if(str1.length == str2.length) {
+      return(true)
+   } else(str1.length != str1.length); {
+      return(false);
+   }
+   } tienenMismaLongitud("hola", "hola")
+      tienenMismaLongitud("no", "sueño")
+
 
 function menosQueNoventa(num) {
-   // Retorna true si el argumento "num" es menor que noventa.
-   // De lo contrario, retorna false.
-   // Tu código:
-}
+   if(num < 90) {
+   return(true)
+   } else {
+      return(false)
+   }
+   } menosQueNoventa(98)
+   menosQueNoventa(50)
 
 function mayorQueCincuenta(num) {
-   // Retorna true si el argumento "num" es mayor que cincuenta.
-   // De lo contrario, retorna false.
-   // Tu código:
-}
+   if(num > 50) { 
+      return(true)
+   } else { 
+      return(false);
+   }
+} mayorQueCincuenta(80)
+mayorQueCincuenta(30)
 
 function esPar(num) {
-   // Retorna true si "num" es par.
-   // De lo contrario, retorna false.
-   // Tu código:
+   if(num%2 === 0) {
+      return (true);
+   }
+   else { 
+   return(false);
+   }
 }
+esPar(8)
+esPar(5)
+
 
 function esImpar(num) {
-   // Retorna true si "num" es impar.
-   // De lo contrario, retorna false.
-   // Tu código:
+   if(num%2 !== 0) {
+      return(true);
+   }
+   else {
+      return(false);
+   }
 }
+esImpar(7)
+esImpar(2)
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
